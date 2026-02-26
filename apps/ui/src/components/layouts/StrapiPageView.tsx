75fe582d2622543bd8d089e4abde2df8e8822a96
Jan Šimeček <simecekjann@gmail.com>
2026-02-26 01:35:51 +0100

    fix: fixed type issue

--- apps/ui/src/components/layouts/StrapiPageView.tsx
+++ apps/ui/src/components/layouts/StrapiPageView.tsx
@@ -42,7 +42,6 @@ export function StrapiPageView({ params, searchParams }: Props) {
         <Container>
           <Breadcrumbs
             breadcrumbs={response?.meta?.breadcrumbs}
-            className="mt-6 mb-6"
             locale={locale}
           />
         </Container>

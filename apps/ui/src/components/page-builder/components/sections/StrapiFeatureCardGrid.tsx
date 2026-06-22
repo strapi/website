c6dd3bb5421938a0ba1c2f04de3725de6ecc55e0
Jan Simecek <simecekjann@gmail.com>
2026-06-22 18:37:32 +0200

    feat: redirects

--- apps/ui/src/components/page-builder/components/sections/StrapiFeatureCardGrid.tsx
+++ apps/ui/src/components/page-builder/components/sections/StrapiFeatureCardGrid.tsx
@@ -21,7 +21,7 @@ export function StrapiFeatureCardGrid({
   readonly component: Data.Component<"sections.feature-card-grid">
 }) {
   return (
-    <Box variant={component.background ?? "none"}>
+    <Box variant={component.background ?? "none"} className="py-16 lg:py-32">
       <Container>
         {component.section && (
           <StrapiSectionHeader component={component.section} />
